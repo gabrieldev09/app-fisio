9904ea18ca5c0c41ff6cbab7414807c881b4a476
Kevyn Murilo <132490286+KevynMurilo@users.noreply.github.com>
2024-06-13 17:54:23 -0300

    Refatoração Paciente

--- src/components/Patient/index.js
+++ src/components/Patient/index.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import Drawer from '../Drawer';
-import back from '../../assets/back.png';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Drawer from "../Drawer";
+import back from "../../assets/back.png";
+
+const patients = [
+  { nome: 'Junior'},
+  { nome: 'Maria'},
+  { nome: 'Pedro'},
+  { nome: 'Julia'},
+  { nome: 'Carlos'},
+];
 
 export default function Patient() {
   return (
     <>
-      <Drawer title='Pacientes' icon={back}/>
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Paciente #1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Paciente #2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Paciente #3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Paciente #4</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Paciente #5</Text>
-        </TouchableOpacity>
+      <Drawer title="Pacientes" icon={back} />
+        <View style={styles.container}>
+          {patients.map((student, nome) => (
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>{student.nome}</Text>
+          </TouchableOpacity>
+          ))}
       </View>
     </>
   );
@@ -31,32 +29,32 @@ export default function Patient() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    marginTop: 50
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginTop: 50,
   },
   header: {
-    width: '100%',
+    width: "100%",
     padding: 20,
-    backgroundColor: '#000066',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#000066",
+    justifyContent: "center",
+    alignItems: "center",
   },
   headerText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 20,
   },
   button: {
-    backgroundColor: '#000066',
+    backgroundColor: "#000066",
     padding: 20,
     marginVertical: 10,
     borderRadius: 10,
-    width: '80%',
-    alignItems: 'center',
+    width: "80%",
+    alignItems: "center",
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
   },
 });
